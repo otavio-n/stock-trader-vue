883e29e3aeb3df2cc596f24122e68629605c49db
Otávio Nishimura <otavio@integra.do>
2023-06-03 18:33:19 -0300

    add portfolio module

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -4,19 +4,22 @@ export default {
   state: {
     stocks: [],
   },
+
   mutations: {
     setStocks(state, stocks) {
       state.stocks = stocks;
     },
   },
+
   actions: {
     buyStock({ commit }, order) {
-      commit();
+      commit('buyStock', order);
     },
     initStocks({ commit }) {
       commit("setStocks", stocks);
     },
   },
+
   getters: {
     stocks(state) {
       return state.stocks;
--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import stocks from "./modules/stocks";
+import portfolio from "./modules/portfolio";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: { stocks },
+  modules: { stocks, portfolio },
 });
