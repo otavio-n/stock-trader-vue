0c42d99f090a3cfeb771fd0cc01390028780d5b2
Otávio Nishimura <otavio@integra.do>
2023-06-05 23:05:34 -0300

    add home layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,26 @@
 <template>
-    <h1>Home</h1>
+  <div>
+    <h1 class="display-3 font-weight-light mb-4">
+      Trade and Consult your Stocks
+    </h1>
+    <v-sheet :elevation="6" class="pa-2 blue darken-2">
+      <v-icon class="white--text mr-3">info</v-icon>
+      <span class="headline font-weight-light">
+        You can Save & Load your Data
+      </span>
+    </v-sheet>
+    <v-sheet :elevation="6" class="pa-2 success mt-3">
+      <v-icon class="white--text mr-3">info</v-icon>
+      <span class="headline font-weight-light">
+        Click on 'Close Day' to start a new day
+      </span>
+    </v-sheet>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style>
-
 </style>
