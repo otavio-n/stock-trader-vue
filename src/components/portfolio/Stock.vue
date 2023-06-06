0c1c8a6db2faa22deacbe1d7a127db2064a72ed0
Otávio Nishimura <otavio@integra.do>
2023-06-05 22:10:53 -0300

    create sell stock component

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,13 +1,59 @@
 <template>
-    <h1>Stocks (Portfolio)</h1>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="blue darken-4 white--text">
+      <v-card-title class="title">
+        <strong>
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }} | Qty: {{ stock.quantity }})</small>
+        </strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field
+          label="Value"
+          type="number"
+          min="0"
+          v-model.number="quantity"
+        ></v-text-field>
+        <v-btn
+          class="blue darken-4 white--text"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+          @click="sellStock"
+        >
+          Sell
+        </v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
-    
-}
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0,
+    };
+  },
+
+  methods: {
+    ...mapActions({ sellStockAction: "sellStock" }),
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity,
+      };
+
+      this.sellStockAction(order);
+      this.quantity = 0;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
